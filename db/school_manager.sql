edcf7dbb64edc6d78236e8f52511602c520f847c
Your Name <mmuzasantuna@gmail.com>
2020-10-02 21:14:34 +0100

    error with tables fixed

--- db/school_manager.sql
+++ db/school_manager.sql
@@ -2,6 +2,7 @@ DROP TABLE bookings;
 DROP TABLE students;
 DROP TABLE classes;
 
+
 CREATE TABLE classes(
     id SERIAL PRIMARY KEY, 
     title VARCHAR(255),
@@ -9,7 +10,7 @@ CREATE TABLE classes(
     date VARCHAR(255),
     duration INT,
     num_students INT
-)
+);
 
 CREATE TABLE students(
     id SERIAL PRIMARY KEY,
@@ -17,10 +18,10 @@ CREATE TABLE students(
     dob VARCHAR(255),
     experience VARCHAR(255),
     class_id INT REFERENCES class(id)
-    )
+);
 
 CREATE TABLE bookings(
-    id SERIAL PRIMARY KEY
-    class_id INT REFERENCES class(id)
+    id SERIAL PRIMARY KEY,
+    class_id INT REFERENCES class(id),
     student_id INT REFERENCES student(id)
-)
+);
