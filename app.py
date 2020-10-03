1a272e954fe11be29ad1889a88d5d66c42416d94
Your Name <mmuzasantuna@gmail.com>
2020-10-03 19:09:53 +0100

    courses template added, courses_bluprint done, courses list route done

--- app.py
+++ app.py
@@ -1,6 +1,10 @@
 from flask import Flask, render_template
 
+from controllers.course_controller import courses_blueprint
+
 app = Flask(__name__)
+
+app.register_blueprint(courses_blueprint)
 
 @app.route("/")
 def main():
--- controllers/course_controller.py
+++ controllers/course_controller.py
@@ -3,5 +3,9 @@ from flask import Blueprint, Flask, redirect, render_template, request
 from models.course import Course
 import repositories.course_repository as course_repository
 
-course_blueprint = Blueprint("course", __name__)
+courses_blueprint = Blueprint("courses", __name__)
 
+@courses_blueprint.route("/courses")
+def courses():
+    courses = course_repository.select_all()
+    return render_template ("courses/index.html", courses = courses)
