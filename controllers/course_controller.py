7fb72536c63943dd4a470eb778e776c8f4784f98
Your Name <mmuzasantuna@gmail.com>
2020-10-04 12:11:16 +0100

    add_new_course template and controller added

--- controllers/course_controller.py
+++ controllers/course_controller.py
@@ -9,3 +9,22 @@ courses_blueprint = Blueprint("courses", __name__)
 def courses():
     courses = course_repository.select_all()
     return render_template ("courses/index.html", courses = courses)
+
+@courses_blueprint.route("/courses/new")
+def new_course():
+    return render_template("courses/new.html")
+
+@courses_blueprint.route("/courses", methods =["POST"])
+def create_course():
+    title = request.form["title"]
+    description = request.form ["description"]
+    date = request.form["date"]
+    duration = request.form["duration"]
+    max_num_students = request.form["max_num_students"]
+
+    course = Course(title, description, date, duration, max_num_students)
+
+    course_repository.save(course)
+
+    return redirect("/courses")
+
