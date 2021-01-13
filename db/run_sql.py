7cbd8d1c7d06292ed0de0f79794bd9e3a63b99d2
Your Name <mmuzasantuna@gmail.com>
2021-01-13 18:26:33 +0000

    changes for database

--- db/run_sql.py
+++ db/run_sql.py
@@ -2,13 +2,16 @@ import os
 import psycopg2
 import psycopg2.extras as ext
 
-DATABASE_URL = os.environ['DATABASE_URL']
+
 
 def run_sql(sql, values = None):
     conn = None
     results = []
 
+    DATABASE_URL = os.environ['DATABASE_URL']
+
     try:
+        conn=psycopg2.connect("dbname='school_manager'") 
         conn=psycopg2.connect(DATABASE_URL, sslmode='require') 
         cur = conn.cursor(cursor_factory=ext.DictCursor)
         cur.execute(sql, values)
