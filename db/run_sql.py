f98ff39591eed1f377927704aafd1058a934192c
Your Name <mmuzasantuna@gmail.com>
2021-01-13 20:03:50 +0000

    database changed to heroku

--- db/run_sql.py
+++ db/run_sql.py
@@ -2,15 +2,15 @@ import os
 import psycopg2
 import psycopg2.extras as ext
 
-# DATABASE_URL = os.environ['DATABASE_URL']
+DATABASE_URL = os.environ['DATABASE_URL']
 
 def run_sql(sql, values = None):
     conn = None
     results = []
 
     try:
-        conn=psycopg2.connect("dbname='school_manager'") 
-        # conn=psycopg2.connect(DATABASE_URL, sslmode='require') 
+        # conn=psycopg2.connect("dbname='school_manager'") 
+        conn=psycopg2.connect(DATABASE_URL, sslmode='require') 
         cur = conn.cursor(cursor_factory=ext.DictCursor)
         cur.execute(sql, values)
         conn.commit()
